4df0c82fbb53c8dcb44f6d251d5b13a7f1fa814b
Raul Bernardeli <raul@raulbcs.com.br>
2018-05-29 15:58:56 -0300

    Update column names

--- proficiencia/SQLs/fact_proficiency_new_cities.sql
+++ proficiencia/SQLs/fact_proficiency_new_cities.sql
@@ -24,14 +24,14 @@ INSERT INTO fact_proficiency_new
     ROUND(SUM( IF(s.level_qualitative = 2,  s.weight , 0)), 2) AS `qualitative_2`,
     ROUND(SUM( IF(s.level_qualitative = 3,  s.weight , 0)), 2) AS `qualitative_3`,
 
-    1 AS disclosure 
+    1 AS disclosure
   FROM
   provabrasil_2015.aggregation_students AS s
   INNER JOIN
     provabrasil_2015.aggregation_tmp AS t
   ON
     t.dependence_id=s.dependence_id AND t.discipline_id=s.discipline_id AND t.grade_id=s.grade_id AND t.localization_id=s.localization_id AND t.state_id=s.state_id AND t.city_id=s.city_id AND t.school_id=0
-  WHERE t.masked=0 AND t.needs_to_be_from_sheet=0 
+  WHERE t.masked=0 AND t.needs_to_be_from_sheet=0
   GROUP BY t.dim_regional_aggregation, t.dim_politic_aggregation;
 
 INSERT INTO fact_proficiency_new
@@ -58,7 +58,7 @@ INSERT INTO fact_proficiency_new
     ROUND(SUM( IF(s.level_qualitative = 2,  s.weight , 0)), 2) AS `qualitative_2`,
     ROUND(SUM( IF(s.level_qualitative = 3,  s.weight , 0)), 2) AS `qualitative_3`,
 
-    1 AS disclosure 
+    1 AS disclosure
   FROM
   provabrasil_2015.aggregation_students AS s
   INNER JOIN
@@ -92,14 +92,14 @@ INSERT INTO fact_proficiency_new
     ROUND(SUM( IF(s.level_qualitative = 2,  s.weight , 0)), 2) AS `qualitative_2`,
     ROUND(SUM( IF(s.level_qualitative = 3,  s.weight , 0)), 2) AS `qualitative_3`,
 
-    1 AS disclosure 
+    1 AS disclosure
   FROM
   provabrasil_2015.aggregation_students AS s
   INNER JOIN
     provabrasil_2015.aggregation_tmp AS t
   ON
     t.dependence_id=0 AND t.discipline_id=s.discipline_id AND t.grade_id=s.grade_id AND t.localization_id=s.localization_id AND t.state_id=s.state_id AND t.city_id=s.city_id AND t.school_id=0
-  WHERE t.masked=0 AND t.needs_to_be_from_sheet=0 AND s.dependence_id<>4 
+  WHERE t.masked=0 AND t.needs_to_be_from_sheet=0 AND s.dependence_id<>4
   GROUP BY t.dim_regional_aggregation, t.dim_politic_aggregation;
 
 INSERT INTO fact_proficiency_new
@@ -126,14 +126,14 @@ INSERT INTO fact_proficiency_new
     ROUND(SUM( IF(s.level_qualitative = 2,  s.weight , 0)), 2) AS `qualitative_2`,
     ROUND(SUM( IF(s.level_qualitative = 3,  s.weight , 0)), 2) AS `qualitative_3`,
 
-    1 AS disclosure 
+    1 AS disclosure
   FROM
   provabrasil_2015.aggregation_students AS s
   INNER JOIN
     provabrasil_2015.aggregation_tmp AS t
   ON
     t.dependence_id=0 AND t.discipline_id=s.discipline_id AND t.grade_id=s.grade_id AND t.localization_id=0 AND t.state_id=s.state_id AND t.city_id=s.city_id AND t.school_id=0
-  WHERE t.masked=0 AND t.needs_to_be_from_sheet=0 AND s.dependence_id<>4 
+  WHERE t.masked=0 AND t.needs_to_be_from_sheet=0 AND s.dependence_id<>4
   GROUP BY t.dim_regional_aggregation, t.dim_politic_aggregation;
 
 -- Masked
@@ -158,13 +158,13 @@ INSERT INTO fact_proficiency_new
       t.grade_id=5,
       IF(
           t.discipline_id=1,
-          r.distribuicao_5ef_LP_nivel_5+r.distribuicao_5ef_LP_nivel_4+r.distribuicao_5ef_LP_nivel_6+r.distribuicao_5ef_LP_nivel_9+r.distribuicao_5ef_LP_nivel_8+r.distribuicao_5ef_LP_nivel_7,
-          r.distribuicao_5ef_MT_nivel_5+r.distribuicao_5ef_MT_nivel_6+r.distribuicao_5ef_MT_nivel_10+r.distribuicao_5ef_MT_nivel_9+r.distribuicao_5ef_MT_nivel_8+r.distribuicao_5ef_MT_nivel_7
+          r.nivel_4_LP5+r.nivel_5_LP5+r.nivel_6_LP5+r.nivel_7_LP5+r.nivel_8_LP5+r.nivel_9_LP5,
+          r.nivel_5_MT5+r.nivel_6_MT5+r.nivel_7_MT5+r.nivel_8_MT5+r.nivel_9_MT5+r.nivel_10_MT5
         ),
       IF(
           t.discipline_id=1,
-          r.distribuicao_9ef_LP_nivel_5+r.distribuicao_9ef_LP_nivel_4+r.distribuicao_9ef_LP_nivel_6+r.distribuicao_9ef_LP_nivel_7+r.distribuicao_9ef_LP_nivel_8,
-          r.distribuicao_9ef_MT_nivel_5+r.distribuicao_9ef_MT_nivel_6+r.distribuicao_9ef_MT_nivel_8+r.distribuicao_9ef_MT_nivel_7+r.distribuicao_9ef_MT_nivel_9
+          r.nivel_4_LP9+r.nivel_5_LP9+r.nivel_6_LP9+r.nivel_7_LP9+r.nivel_8_LP9,
+          r.nivel_5_MT9+r.nivel_6_MT9+r.nivel_7_MT9+r.nivel_8_MT9+r.nivel_9_MT9
         )
     ), 2) AS `level_optimal`,
 
@@ -173,64 +173,64 @@ INSERT INTO fact_proficiency_new
       t.grade_id=5,
       IF(
           t.discipline_id=1,
-          r.distribuicao_5ef_LP_ate_nivel_1, 
-          r.distribuicao_5ef_MT_nivel_2+r.distribuicao_5ef_MT_nivel_1+r.distribuicao_5ef_MT_abaixo_nivel_1
+          r.ate_nivel_1_LP5,
+          r.nivel_2_MT5+r.nivel_1_MT5+r.nivel_0_MT5
         ),
       IF(
           t.discipline_id=1,
-          r.distribuicao_9ef_LP_abaixo_nivel_1,
-          r.distribuicao_9ef_MT_nivel_1+r.distribuicao_9ef_MT_abaixo_nivel_1
+          r.nivel_0_LP9,
+          r.nivel_0_MT9+r.nivel_1_MT9
         )
     ), 2) AS `qualitative_0`,
     ROUND(IF(
       t.grade_id=5,
       IF(
           t.discipline_id=1,
-          r.distribuicao_5ef_LP_nivel_2+r.distribuicao_5ef_LP_nivel_3,
-          r.distribuicao_5ef_MT_nivel_3+r.distribuicao_5ef_MT_nivel_4
+          r.nivel_2_LP5+r.nivel_3_LP5,
+          r.nivel_3_MT5+r.nivel_4_MT5
         ),
       IF(
           t.discipline_id=1,
-          r.distribuicao_9ef_LP_nivel_3+r.distribuicao_9ef_LP_nivel_2+r.distribuicao_9ef_LP_nivel_1,
-          r.distribuicao_9ef_MT_nivel_2+r.distribuicao_9ef_MT_nivel_3+r.distribuicao_9ef_MT_nivel_4
+          r.nivel_1_LP9+r.nivel_2_LP9+r.nivel_3_LP9,
+          r.nivel_2_MT9+r.nivel_3_MT9+r.nivel_4_MT9
         )
     ), 2) AS `qualitative_1`,
     ROUND(IF(
       t.grade_id=5,
       IF(
           t.discipline_id=1,
-          r.distribuicao_5ef_LP_nivel_5+r.distribuicao_5ef_LP_nivel_4,
-          r.distribuicao_5ef_MT_nivel_5+r.distribuicao_5ef_MT_nivel_6
+          r.nivel_4_LP5+r.nivel_5_LP5,
+          r.nivel_5_MT5+r.nivel_6_MT5
         ),
       IF(
           t.discipline_id=1,
-          r.distribuicao_9ef_LP_nivel_5+r.distribuicao_9ef_LP_nivel_4,
-          r.distribuicao_9ef_MT_nivel_5+r.distribuicao_9ef_MT_nivel_6
+          r.nivel_4_LP9+r.nivel_5_LP9,
+          r.nivel_5_MT9+r.nivel_6_MT9
         )
     ), 2) AS `qualitative_2`,
     ROUND(IF(
       t.grade_id=5,
       IF(
           t.discipline_id=1,
-          r.distribuicao_5ef_LP_nivel_6+r.distribuicao_5ef_LP_nivel_9+r.distribuicao_5ef_LP_nivel_8+r.distribuicao_5ef_LP_nivel_7,
-          r.distribuicao_5ef_MT_nivel_10+r.distribuicao_5ef_MT_nivel_9+r.distribuicao_5ef_MT_nivel_8+r.distribuicao_5ef_MT_nivel_7
+          r.nivel_6_LP5+r.nivel_7_LP5+r.nivel_8_LP5+r.nivel_9_LP5,
+          r.nivel_7_MT5+r.nivel_8_MT5+r.nivel_9_MT5+r.nivel_10_MT5
         ),
       IF(
           t.discipline_id=1,
-          r.distribuicao_9ef_LP_nivel_6+r.distribuicao_9ef_LP_nivel_7+r.distribuicao_9ef_LP_nivel_8,
-          r.distribuicao_9ef_MT_nivel_8+r.distribuicao_9ef_MT_nivel_7+r.distribuicao_9ef_MT_nivel_9
+          r.nivel_6_LP9+r.nivel_7_LP9+r.nivel_8_LP9,
+          r.nivel_7_MT9+r.nivel_8_MT9+r.nivel_9_MT9
         )
     ), 2) AS `qualitative_3`,
 
-    4 AS disclosure 
-  FROM 
+    4 AS disclosure
+  FROM
     provabrasil_2015.aggregation_tmp AS t
-  INNER JOIN 
-    provabrasil_2015.resultados_municipios AS r 
+  INNER JOIN
+    provabrasil_2015.resultados_municipios AS r
   ON
     r.dependence_id = t.dependence_id
                 AND r.localization_id = t.localization_id
-                AND r.ibge_id = t.ID_MUNICIPIO 
-  WHERE t.masked=0 AND t.needs_to_be_from_sheet=1 
+                AND r.ibge_id = t.ID_MUNICIPIO
+  WHERE t.masked=0 AND t.needs_to_be_from_sheet=1
   GROUP BY t.dim_regional_aggregation, t.dim_politic_aggregation;
   COMMIT;
